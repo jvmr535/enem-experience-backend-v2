18655885ee0e680bbc341a785f1f89732c0207ec
Joao Rosa <JoaoVitor.DeMouraRosa-ext@ab-Inbev.com>
2025-10-02 23:14:08 -0300

    feat:

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../src/generated/prisma/wasm'
 import { PrismaD1 } from '@prisma/adapter-d1'
 import type { D1Database } from '@cloudflare/workers-types'
 
 const prismaClients = {
   async fetch(db: D1Database) {
     const adapter = new PrismaD1(db)
-    const prisma = new PrismaClient({ adapter })
+    const prisma = new PrismaClient({ 
+      adapter,
+      log: ['error'],
+    })
     return prisma
   },
 }
